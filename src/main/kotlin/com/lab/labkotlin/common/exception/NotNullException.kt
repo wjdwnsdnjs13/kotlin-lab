1e1c6616dfc652ea2217654c2144a1006d7c326c
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2025-08-12 14:50:56 +0900

    feat : 커스텀 Exception 추가

---
+++ src/main/kotlin/com/lab/labkotlin/common/exception/NotNullException.kt
@@ -0,0 +1,14 @@
+package com.lab.labkotlin.common.exception
+
+import org.springframework.http.HttpStatus
+
+class NotNullException(
+    val httpStatusCode: HttpStatus = HttpStatus.BAD_REQUEST,
+    val code: String = "NOT_NULL-400-01",
+    val fieldName: String,
+    ) : ErrorCode{
+    override fun getStatus(): HttpStatus = httpStatusCode
+    override fun getCode(): String = code
+    override fun getMessage(): String =
+        "필드 ${fieldName}의 값이 누락되었거나 형식이 올바르지 않습니다."
+}
