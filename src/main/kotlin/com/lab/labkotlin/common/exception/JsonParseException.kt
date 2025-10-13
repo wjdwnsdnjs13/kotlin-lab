e7d0a5b65ac146950589dc49f311accfdc6ac3e9
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2025-10-13 13:02:23 +0900

    refactor : 에러 코드 수정

--- src/main/kotlin/com/lab/labkotlin/common/exception/JsonParseException.kt
+++ src/main/kotlin/com/lab/labkotlin/common/exception/JsonParseException.kt
@@ -4,10 +4,10 @@ import org.springframework.http.HttpStatus
 
 class JsonParseException(
     val httpStatusCode: HttpStatus = HttpStatus.BAD_REQUEST,
-    val code: String = "JSON-400-01",
-    val message: String = "JSON 파싱 오류가 발생했습니다.",
+    val errorCode: String = "JSON-400-01",
+    val errorMessage: String = "JSON 파싱 오류가 발생했습니다.",
     ) : ErrorCode{
     override fun getStatus(): HttpStatus = httpStatusCode
-    override fun getCode(): String = code
-    override fun getMessage(): String = message
+    override fun getCode(): String = errorCode
+    override fun getMessage(): String = errorMessage
 }
