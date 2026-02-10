21f8d5411fe95eba1bbeb1db47b6b92a1aca57c7
junwon <67488973+wjdwnsdnjs13@users.noreply.github.com>
2026-02-10 15:32:07 +0900

    feat : Mailgun SDK Email Sender 추가

---
+++ src/main/kotlin/com/lab/labkotlin/application/MailgunSdkEmailSender.kt
@@ -0,0 +1,39 @@
+package com.lab.labkotlin.application
+
+import com.lab.labkotlin.application.email.EmailRequest
+import com.mailgun.api.v3.MailgunMessagesApi
+import com.mailgun.client.MailgunClient
+import com.mailgun.model.message.Message
+import com.mailgun.model.message.MessageResponse
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class MailgunSdkEmailSender(
+    @Value("\${mailgun.api-key}")
+    private val apiKey: String,
+    @Value("\${spring.mail.username}")
+    private val domain: String,
+) : EmailSender {
+    private val mailgunMessagesApi: MailgunMessagesApi =
+        MailgunClient.config(apiKey)
+            .createApi(MailgunMessagesApi::class.java)
+
+    private val logger = LoggerFactory.getLogger(MailgunSdkEmailSender::class.java)
+
+    override suspend fun send(emailRequest: EmailRequest) {
+        logger.info("Mailgun SDK Email 전송 시작: $emailRequest")
+        val resonse: MessageResponse =
+            mailgunMessagesApi.sendMessage(
+                emailRequest.from,
+                Message.builder()
+                    .from(emailRequest.from)
+                    .to(emailRequest.to)
+                    .subject(emailRequest.subject)
+                    .text(emailRequest.content)
+                    .build(),
+            )
+        logger.info("Mailgun SDK Email 전송 결과: $resonse")
+    }
+}
